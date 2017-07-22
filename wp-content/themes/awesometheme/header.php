8a4b4697edaeecaed654d012cc4436adb907b053
osviatka <svyatka@ukr.net>
2017-07-22 22:51:20 +0300

    customizer

--- wp-content/themes/awesometheme/header.php
+++ wp-content/themes/awesometheme/header.php
@@ -35,3 +35,5 @@ endif;
 <body <?php body_class( $awesome_classes ); ?>>
 
     <?php wp_nav_menu(array('theme_location'=>'primary')); ?>
+
+    <img src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />

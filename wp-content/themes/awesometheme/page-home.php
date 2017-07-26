19e78b6b218c9e93322a535f83c6252f8f9fd68e
osviatka <svyatka@ukr.net>
2017-07-26 18:29:02 +0300

    upgrade page-home

--- wp-content/themes/awesometheme/page-home.php
+++ wp-content/themes/awesometheme/page-home.php
@@ -6,13 +6,21 @@
 
             <?php
 
-                $lastBlog = new WP_Query('type=post&posts_per_page=1');
+            $lastBlog = new WP_Query('type=post&posts_per_page=1');
 
-                if ($lastBlog->have_posts()):
+            if( $lastBlog->have_posts() ):
 
-                    while ( $lastBlog->have_posts()) : $lastBlog->the_post();  ?>
+                while( $lastBlog->have_posts() ): $lastBlog->the_post(); ?>
 
-                        
+                    <?php get_template_part('content',get_post_format()); ?>
+
+                <?php endwhile;
+
+            endif;
+
+            wp_reset_postdata();
+
+            ?>
         </div>
 
             <div class="col-xs-12 col-sm-8">
@@ -29,11 +37,29 @@
 
             endif;
 
+            //PRINT OTHER 2 POSTS NOT THE FIRST ONE
+
+
+            $lastBlog = new WP_Query('type=post&posts_per_page=2&offset=1');
+
+            if( $lastBlog->have_posts() ):
+
+                while( $lastBlog->have_posts() ): $lastBlog->the_post(); ?>
+
+                    <?php get_template_part('content',get_post_format()); ?>
+
+                <?php endwhile;
+
+            endif;
+
+            wp_reset_postdata();
+
             ?>
 
         </div>
 
         <div class="col-xs-12 col-sm-4">
+
             <?php get_sidebar(); ?>
         </div>
 
